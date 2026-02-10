e2cfce4e4b21fcb228e1eebd7e5f47f37cd71b3c
real-aminmaghfuri <real.aminmaghfuri@gmail.com>
2026-02-10 13:35:08 +0700

    feat(build): Ignore TS and ESLint errors during build
    Disables `ignoreDuringBuilds` for ESLint and `ignoreBuildErrors` for TypeScript in both the website and system Next.js configurations. This prevents build failures due to linting or type errors, allowing for faster iteration during development. The `ignore-build` script has also been updated to reflect this change.

--- apps/system/next.config.js
+++ apps/system/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   transpilePackages: ["ui", "shared", "data"],
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
