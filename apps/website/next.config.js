90f733f0f41ab2377be49e992261e49fb02cc193
real-aminmaghfuri <real.aminmaghfuri@gmail.com>
2026-02-11 09:58:54 +0700

    feat(build): Update Next.js to latest and optimize build scripts
    Updates the Next.js version to 'latest' in both website and system apps.
    Introduces a new 'ignore-build' script to bypass ESLint and TypeScript checks during production builds, aligning with existing `next.config.js` settings for build errors and ESLint ignores. This ensures smoother and faster builds.

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -13,15 +13,9 @@ const nextConfig = {
     ],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
