f344a6faf06b222917e4ae5253f1db69d5e78982
real-aminmaghfuri <real.aminmaghfuri@gmail.com>
2026-04-11 11:35:03 +0700

    feat(next.config): Allow remote image hostnames
    Configure Next.js to allow images from Cloudinary and Unsplash to be displayed. This is necessary for image optimization and proper rendering of external image sources.

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui", "shared"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
